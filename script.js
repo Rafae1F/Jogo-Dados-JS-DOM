e56bb207c559f42995ef9b27afdcb9e0b78cdcc2
Rafael Ferreira <64290383+Rafae1F@users.noreply.github.com>
2023-01-14 21:24:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-function diceGame(){
+function jogarDados(){
 	
-	//gera número random para o dado
-	let diceRandomX = Math.floor(Math.random() * 6 +1);
-    let diceRandomY = Math.floor(Math.random() * 6 + 1);
+    //gera número random para o dado
+    let dadoAleatorioX = Math.floor(Math.random() * 6) + 1;
+    let dadoAleatorioY = Math.floor(Math.random() * 6) + 1;
 
 
     //carrega a imagem do dado com o número random gerado.
-    document.querySelectorAll("img")[0].setAttribute("src", "images/dice" + diceRandomX + ".png");
-    document.querySelectorAll("img")[1].setAttribute("src", "images/dice" + diceRandomY + ".png"); 
+    document.querySelectorAll("img")[0].setAttribute("src", "images/dice" + dadoAleatorioX + ".png");
+    document.querySelectorAll("img")[1].setAttribute("src", "images/dice" + dadoAleatorioY + ".png"); 
 
 
     //Verificação do resultado.
-	if (diceRandomX > diceRandomY) {
-		document.querySelector("h1").textContent = "Player 1 ganhou!";
-	} else if (diceRandomX < diceRandomY) {
-		document.querySelector("h1").textContent = "Player 2 ganhou!";
+	if (dadoAleatorioX > dadoAleatorioY) {
+		document.querySelector("h1").textContent = "Jogador 1 ganhou!";
+	} else if (dadoAleatorioX < dadoAleatorioY) {
+		document.querySelector("h1").textContent = "Jogador 2 ganhou!";
 	} else {
 		document.querySelector("h1").textContent = "Empate";
 	}
